e48fa7488d7ee5168b1afb066e5ff2f867d15dd4
r.ishibashi <ryuhei.ishibashi@bizreach.co.jp>
2018-12-07 18:50:26 +0900

    rename class

--- src/test/scala/computerdatabase/S3PutSimulation.scala
+++ src/test/scala/computerdatabase/S3PutSimulation.scala
@@ -13,26 +13,27 @@ import java.io
 class S3PutSimulation extends Simulation {
 
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(new S3Put)
+    .exec(new CustomActionBuilder(() => {
+    println("***hoge***")
+    Thread.sleep((1000 * Math.random()).toInt)
+    (OK, Some("ok"))
+  }))
 
   setUp(scn.inject(atOnceUsers(5)))
 }
 
-class S3Put extends ActionBuilder {
-  override def build(ctx: ScenarioContext, next: Action): Action = new S3PutAction(ctx.coreComponents.statsEngine, next)
+class CustomActionBuilder(func: () => (OK.type, Some[String])) extends ActionBuilder {
+  override def build(ctx: ScenarioContext, next: Action): Action = new CustomAction(ctx.coreComponents.statsEngine, func, next)
 }
 
-class S3PutAction(val statsEngine: StatsEngine, val next: Action) extends ExitableAction with NameGen {
+class CustomAction(val statsEngine: StatsEngine, func: () => (OK.type, Some[String]), val next: Action) extends ExitableAction with NameGen {
   override def name: String = genName("Hoge")
 
   override def execute(session: Session) = {
     val start = System.currentTimeMillis
 
-    val status = try {
-      //TODO S3にPUTする
-      println("***hoge***")
-      Thread.sleep((1000 * Math.random()).toInt)
-      (OK, Some("ok"))
+    val result = try {
+      func()
     } catch {
       case _: Throwable => (KO, Some("ng"))
     }
@@ -40,7 +41,7 @@ class S3PutAction(val statsEngine: StatsEngine, val next: Action) extends Exitab
 
     val end = System.currentTimeMillis
     val timings = ResponseTimings(start, end)
-    statsEngine.logResponse(session, name, timings, status._1, status._2, None)
+    statsEngine.logResponse(session, name, timings, result._1, result._2, None)
     next ! session
   }
 }
